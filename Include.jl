0ccc965b7c0e968ef77d68ed8169746a753f47a3
lwdesilva <lwd37@cornell.edu>
2023-12-12 12:56:49 -0500

    Code runs

--- Include.jl
+++ Include.jl
@@ -5,4 +5,32 @@ const _ROOT = pwd();
 import Pkg; Pkg.activate("."); Pkg.instantiate(); Pkg.update()
 
 # load external packages -
+using VLDecisionsPackage 
 using VLDecisionsPackage
+using CSV
+using DataFrames
+using ForwardDiff
+using PrettyTables
+using Plots
+using Colors
+using Distributions
+using JLD2
+using FileIO
+using Statistics
+using LinearAlgebra
+using MathOptInterface
+using DataStructures
+using Flux
+
+# load my color palette -
+colors = Dict{Int,RGB}();
+colors[1] = colorant"#0077BB"
+colors[2] = colorant"#88CCEE"
+colors[3] = colorant"#EE7733"
+colors[4] = colorant"#CC3311"
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
+include(joinpath(_PATH_TO_SRC, "Files.jl"));
+include(joinpath(_PATH_TO_SRC, "Indifference.jl"));
+include(joinpath(_PATH_TO_SRC, "Budget.jl"));
